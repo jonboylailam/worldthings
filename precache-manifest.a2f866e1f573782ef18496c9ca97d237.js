36217c5fd5fa653590115fe811b8e0d8fa9fa14a
Jon Lai Lam <jon@lailam.co.za>
2019-05-01 18:26:55 +0200

    Updates

---
+++ precache-manifest.a2f866e1f573782ef18496c9ca97d237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73522b848fd0aceb1921a6f664dc13a2",
+    "url": "/worldthings/index.html"
+  },
+  {
+    "revision": "796f0150c71c6c38270a",
+    "url": "/worldthings/static/css/main.eb14524e.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/worldthings/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "796f0150c71c6c38270a",
+    "url": "/worldthings/static/js/main.2d097482.chunk.js"
+  },
+  {
+    "revision": "b71ee69b6b1e024df170",
+    "url": "/worldthings/static/js/runtime~main.2bd9c42f.js"
+  }
+]);
